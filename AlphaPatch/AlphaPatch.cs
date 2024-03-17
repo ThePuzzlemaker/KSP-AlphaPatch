4a9227cf6754f7bb1b4cc8dabf2ae6fa78d44294
ThePuzzlemaker <tpzker@thepuzzlemaker.info>
2024-03-17 16:16:35 -0500

    Initial commit

---
+++ AlphaPatch/AlphaPatch.cs
@@ -0,0 +1,90 @@
+﻿#pragma warning disable IDE0051
+#pragma warning disable IDE0060
+#pragma warning disable IDE0044
+using System;
+using System.IO;
+using HarmonyLib;
+using static UrlDir;
+using System.Collections.Generic;
+using Mono.Cecil;
+using BepInEx.Logging;
+
+namespace AlphaPatch
+{
+    public class AlphaPatch
+    {
+        public static IEnumerable<string> TargetDLLs { get; } = new[] { "Assembly-CSharp.dll" };
+
+        public static void Finish()
+        {
+            var harmony = new Harmony("info.thepuzzlemaker.alphapatch");
+            harmony.PatchAll();
+            Logger.CreateLogSource(nameof(AlphaPatch)).LogInfo("=== AlphaPatch Initialized ===");
+        }
+
+        public static void Patch(AssemblyDefinition assembly) { }
+    }
+
+    [HarmonyPatch(typeof(UrlDir), MethodType.Constructor)]
+    [HarmonyPatch(new Type[] { typeof(ConfigDirectory[]), typeof(ConfigFileType[]) })]
+    class UrlDirPatch01
+    {
+        static AccessTools.FieldRef<UrlDir, List<UrlFile>> _files = AccessTools.FieldRefAccess<UrlDir, List<UrlFile>>("_files");
+        static AccessTools.FieldRef<UrlDir, List<UrlDir>> _children = AccessTools.FieldRefAccess<UrlDir, List<UrlDir>>("_children");
+        
+        static void Postfix(UrlDir __instance, ConfigDirectory[] dirConfig, ConfigFileType[] fileConfig)
+        {
+            _files(__instance).Sort(delegate (UrlFile a, UrlFile b)
+            {
+                return a.name.CompareTo(b.name);
+            });
+            _children(__instance).Sort(delegate (UrlDir a, UrlDir b)
+            {
+                return a.name.CompareTo(b.name);
+            });            
+        }
+    }
+
+    [HarmonyPatch(typeof(UrlDir), "Create")]
+    [HarmonyPatch(new Type[] { typeof(UrlDir), typeof(DirectoryInfo) })]
+    class UrlDirPatch02
+    {
+        static AccessTools.FieldRef<UrlDir, List<UrlFile>> _files = AccessTools.FieldRefAccess<UrlDir, List<UrlFile>>("_files");
+        static AccessTools.FieldRef<UrlDir, List<UrlDir>> _children = AccessTools.FieldRefAccess<UrlDir, List<UrlDir>>("_children");
+
+        static void Postfix(UrlDir __instance, UrlDir parent, DirectoryInfo info)
+        {
+            _files(__instance).Sort(delegate (UrlFile a, UrlFile b)
+            {
+                return a.name.CompareTo(b.name);
+            });
+            _children(__instance).Sort(delegate (UrlDir a, UrlDir b)
+            {
+                return a.name.CompareTo(b.name);
+            });
+        }
+    }
+
+    [HarmonyPatch(typeof(UrlDir), "CreateDirectory")]
+    [HarmonyPatch(new Type[] { typeof(UrlIdentifier), typeof(int) })]
+    class UrlDirPatch03
+    {
+        static AccessTools.FieldRef<UrlDir, List<UrlFile>> _files = AccessTools.FieldRefAccess<UrlDir, List<UrlFile>>("_files");
+        static AccessTools.FieldRef<UrlDir, List<UrlDir>> _children = AccessTools.FieldRefAccess<UrlDir, List<UrlDir>>("_children");
+
+        static void Postfix(UrlDir __instance, UrlIdentifier id, int index, ref UrlDir __result)
+        {
+            if (__result != null)
+            {
+                _files(__instance).Sort(delegate (UrlFile a, UrlFile b)
+                {
+                    return a.name.CompareTo(b.name);
+                });
+                _children(__instance).Sort(delegate (UrlDir a, UrlDir b)
+                {
+                    return a.name.CompareTo(b.name);
+                });
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+# AlphaPatch for KSP 1.12
+
+This patch fixes out-of-order loading of assemblies and config files, causing issues with ModuleManager. This fix creates the proper behaviour on all filesystems, including those that do not alphabetically sort directory listings, such as BTRFS and FAT32.
+
+# Installation
+
+1. Download [BepInEx v5.4.22](https://github.com/BepInEx/BepInEx/releases/tag/v5.4.22) from its GitHub page. Extract into the KSP directory. Ensure that `winhttp.dll` is replaced.
+2. Start the game, then close it. It does not need to load all the way, only to the point where the Squad logo shows.
+3. Download (or build) the `AlphaPatch.dll` file and place it in `BepInEx/patchers` directory.
+
+## Why BepInEx?
+
+This project uses BepInEx because KSP's stock modding facilities do not allow the DLL to be loaded early enough to stop out-of-order patching.
+
+BepInEx's preloader ability allows AlphaPatch to be loaded before anything in KSP, allowing it to patch the file loading code before it can load files out of order.
+
+# License
+
+This overall source tree is based on [HarmonyKSP][1], licensed under MIT.
+
+This project is licensed under the MIT license.
+
+[1]: https://github.com/KSPModdingLibs/HarmonyKSP/
