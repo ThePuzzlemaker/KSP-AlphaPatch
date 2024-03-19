3ea5ce102a9cd01a18f42e6d07837f7b47e89f82
ThePuzzlemaker <tpzker@thepuzzlemaker.info>
2024-03-18 22:43:44 -0500

    Fix by using doorstop directly

--- AlphaPatch/AlphaPatch.cs
+++ AlphaPatch/AlphaPatch.cs
@@ -1,93 +1,226 @@
-﻿#pragma warning disable IDE0051
-#pragma warning disable IDE0060
-#pragma warning disable IDE0044
+﻿#pragma warning disable
 using System;
 using System.IO;
 using HarmonyLib;
-using static UrlDir;
 using System.Collections.Generic;
-using Mono.Cecil;
-using BepInEx.Logging;
+using System.Reflection;
+using static UrlDir;
 
 namespace AlphaPatch
 {
     public class AlphaPatch
     {
-        public static IEnumerable<string> TargetDLLs { get; } = new[] { "Assembly-CSharp.dll" };
 
-        public static void Finish()
+        public static void Main()
         {
             var harmony = new Harmony("info.thepuzzlemaker.alphapatch");
-            harmony.PatchAll();
-            Logger.CreateLogSource(nameof(AlphaPatch)).LogInfo("=== AlphaPatch Initialized ===");
-        }
-
-        public static void Patch(AssemblyDefinition assembly) { }
-    }
-
-    [HarmonyPatch(typeof(UrlDir), MethodType.Constructor)]
-    [HarmonyPatch(new Type[] { typeof(ConfigDirectory[]), typeof(ConfigFileType[]) })]
-    class UrlDirPatch01
-    {
-        static AccessTools.FieldRef<UrlDir, List<UrlFile>> _files = AccessTools.FieldRefAccess<UrlDir, List<UrlFile>>("_files");
-        static AccessTools.FieldRef<UrlDir, List<UrlDir>> _children = AccessTools.FieldRefAccess<UrlDir, List<UrlDir>>("_children");
-        
-        static bool Postfix(UrlDir __instance, ConfigDirectory[] dirConfig, ConfigFileType[] fileConfig)
-        {
-            _files(__instance).Sort(delegate (UrlFile a, UrlFile b)
-            {
-                return a.name.CompareTo(b.name);
-            });
-            _children(__instance).Sort(delegate (UrlDir a, UrlDir b)
-            {
-                return a.name.CompareTo(b.name);
-            });  
-	    return true;
+            
+            Console.WriteLine("=== AlphaPatch Initialized ===");
+            Console.WriteLine("Patching UrlDir constructor...");
+            var urlDirCtor = AccessTools.Constructor(typeof(UrlDir), new Type[] { typeof(ConfigDirectory[]), typeof(ConfigFileType[]) });
+            MethodInfo urlDirCtorPrefix = AccessTools.Method(typeof(UrlDirPatch), nameof(UrlDirPatch.Patch01));
+            harmony.Patch(urlDirCtor, new HarmonyMethod(urlDirCtorPrefix));
+            
+            Console.WriteLine("Patching UrlDir.Create...");
+            var urlDirCreate = AccessTools.Method(typeof(UrlDir), "Create", new Type[] { typeof(UrlDir), typeof(DirectoryInfo) });
+            MethodInfo urlDirCreatePrefix = AccessTools.Method(typeof(UrlDirPatch), nameof(UrlDirPatch.Patch02));
+            harmony.Patch(urlDirCreate, new HarmonyMethod(urlDirCreatePrefix));
+            
+            Console.WriteLine("Patching UrlDir.CreateDirectory...");
+            var urlDirCreateDirectory = AccessTools.Method(typeof(UrlDir), "CreateDirectory", new Type[] { typeof(UrlIdentifier), typeof(int) });
+            MethodInfo urlDirCreateDirectoryPrefix = AccessTools.Method(typeof(UrlDirPatch), nameof(UrlDirPatch.Patch03));
+            harmony.Patch(urlDirCreateDirectory, new HarmonyMethod(urlDirCreateDirectoryPrefix));
+            
+            Console.WriteLine("Patching UrlDir.GetConfig...");
+            var urlDirGetConfig = AccessTools.Method(typeof(UrlDir), "GetConfig", new Type[] { typeof(UrlIdentifier), typeof(int) });
+            MethodInfo urlDirGetConfigPrefix = AccessTools.Method(typeof(UrlDirPatch), nameof(UrlDirPatch.Patch04));
+            harmony.Patch(urlDirGetConfig, new HarmonyMethod(urlDirGetConfigPrefix));
         }
     }
 
-    [HarmonyPatch(typeof(UrlDir), "Create")]
-    [HarmonyPatch(new Type[] { typeof(UrlDir), typeof(DirectoryInfo) })]
-    class UrlDirPatch02
+    class UrlDirPatch
     {
         static AccessTools.FieldRef<UrlDir, List<UrlFile>> _files = AccessTools.FieldRefAccess<UrlDir, List<UrlFile>>("_files");
         static AccessTools.FieldRef<UrlDir, List<UrlDir>> _children = AccessTools.FieldRefAccess<UrlDir, List<UrlDir>>("_children");
+        static AccessTools.FieldRef<UrlDir, string> _path = AccessTools.FieldRefAccess<UrlDir, string>("_path");
+        static AccessTools.FieldRef<UrlDir, UrlDir> _root = AccessTools.FieldRefAccess<UrlDir, UrlDir>("_root");
+        static AccessTools.FieldRef<UrlDir, UrlDir> _parent = AccessTools.FieldRefAccess<UrlDir, UrlDir>("_parent");
+        static AccessTools.FieldRef<UrlDir, string> _name = AccessTools.FieldRefAccess<UrlDir, string>("_name");
 
-        static bool Postfix(UrlDir __instance, UrlDir parent, DirectoryInfo info)
+        public static bool Patch01(UrlDir __instance, ref ConfigDirectory[] dirConfig, ConfigFileType[] fileConfig)
         {
-            _files(__instance).Sort(delegate (UrlFile a, UrlFile b)
-            {
-                return a.name.CompareTo(b.name);
+            _files(__instance) = new List<UrlFile>();
+            _children(__instance) = new List<UrlDir>();
+            _parent(__instance) = null;
+            _root(__instance) = __instance;
+            _name(__instance) = "root";
+            List<ConfigDirectory> dirConfig1 = new List<ConfigDirectory>(dirConfig);
+            dirConfig1.Sort(delegate (ConfigDirectory a, ConfigDirectory b) {
+                return a.directory.CompareTo(b.directory);
             });
-            _children(__instance).Sort(delegate (UrlDir a, UrlDir b)
+            int i = 0;
+            int num = dirConfig1.Count;
+            while (i < num)
             {
-                return a.name.CompareTo(b.name);
-            });
-	    return true;
-        }
-    }
-
-    [HarmonyPatch(typeof(UrlDir), "CreateDirectory")]
-    [HarmonyPatch(new Type[] { typeof(UrlIdentifier), typeof(int) })]
-    class UrlDirPatch03
-    {
-        static AccessTools.FieldRef<UrlDir, List<UrlFile>> _files = AccessTools.FieldRefAccess<UrlDir, List<UrlFile>>("_files");
-        static AccessTools.FieldRef<UrlDir, List<UrlDir>> _children = AccessTools.FieldRefAccess<UrlDir, List<UrlDir>>("_children");
-
-        static bool Postfix(UrlDir __instance, UrlIdentifier id, int index, ref UrlDir __result)
-        {
-            if (__result != null)
-            {
-                _files(__instance).Sort(delegate (UrlFile a, UrlFile b)
-                {
-                    return a.name.CompareTo(b.name);
-                });
-                _children(__instance).Sort(delegate (UrlDir a, UrlDir b)
-                {
-                    return a.name.CompareTo(b.name);
-                });
+                var inst = (UrlDir)Activator.CreateInstance(typeof(UrlDir), BindingFlags.Instance | BindingFlags.NonPublic, null, new object[] { __instance, dirConfig1[i] }, null, null);
+                _children(__instance).Add(inst);
+                i++;
             }
-	    return true;
+            foreach (UrlFile urlFile in __instance.AllFiles)
+            {
+                urlFile.ConfigureFile(fileConfig);
+            }
+            _children(__instance).Sort(delegate (UrlDir a, UrlDir b)
+            {
+                return a.name.CompareTo(b.name);
+            });
+            return false; // Skip the original method.
+        }
+
+        public static bool Patch02(UrlDir __instance, UrlDir parent, DirectoryInfo info)
+        {
+            _path(__instance) = info.FullName;
+            _parent(__instance) = parent;
+            _root(__instance) = parent.root;
+            _files(__instance) = new List<UrlFile>();
+            _children(__instance) = new List<UrlDir>();
+            FileInfo[] files0 = info.GetFiles();
+            List<FileInfo> files = new List<FileInfo>(files0);
+            files.Sort(delegate (FileInfo a, FileInfo b) {
+                return a.Name.CompareTo(b.Name);
+            });
+            int i = 0;
+            int num = files.Count;
+            while (i < num)
+            {
+                _files(__instance).Add(new UrlFile(__instance, files[i]));
+                i++;
+            }
+            DirectoryInfo[] directories0 = info.GetDirectories();
+            List<DirectoryInfo> directories = new List<DirectoryInfo>(directories0);
+            directories.Sort(delegate (DirectoryInfo a, DirectoryInfo b) {
+                return a.Name.CompareTo(b.Name);
+            });
+            int j = 0;
+            int num2 = directories.Count;
+            while (j < num2)
+            {
+                DirectoryInfo directoryInfo = directories[j];
+                if (directoryInfo.Name != ".svn" && directoryInfo.Name != "PluginData" && directoryInfo.Name != "zDeprecated")
+                {
+                    var inst = (UrlDir)Activator.CreateInstance(typeof(UrlDir), BindingFlags.Instance | BindingFlags.NonPublic, null, new object[] { __instance, directoryInfo }, null, null);
+                    _children(__instance).Add(inst);
+                }
+                j++;
+            }
+            _children(__instance).Sort(delegate (UrlDir a, UrlDir b)
+            {
+                return a.name.CompareTo(b.name);
+            });
+            return false; // Skip the original method
+        }
+
+        public static bool Patch03(UrlDir __instance, UrlIdentifier id, int index, ref UrlDir __result)
+        {
+            if (index == id.urlDepth)
+            {
+                int i = 0;
+                int count = _children(__instance).Count;
+                while (i < count)
+                {
+                    if (id[index] == _children(__instance)[i].name)
+                    {
+                        __result = _children(__instance)[i];
+                        return false; // Skip the original method.
+                    }
+                    i++;
+                }
+                DirectoryInfo directoryInfo = Directory.CreateDirectory(Path.Combine(_path(__instance), id[index]));
+                var urlDir = (UrlDir)Activator.CreateInstance(typeof(UrlDir), BindingFlags.Instance | BindingFlags.NonPublic, null, new object[] { __instance, directoryInfo }, null, null);
+                _children(__instance).Add(urlDir);
+                _children(__instance).Sort(delegate (UrlDir a, UrlDir b) {
+                    return _name(a).CompareTo(_name(b));
+                });
+                __result = urlDir;
+                return false; // Skip the original method.
+            }
+            int j = 0;
+            int count2 = _children(__instance).Count;
+            while (j < count2)
+            {
+                if (id[index] == _children(__instance)[j].name)
+                {
+                    var dir = _children(__instance)[j];
+                    var createDirectory = typeof(UrlDir).GetMethod("CreateDirectory", BindingFlags.Instance | BindingFlags.NonPublic, null, new Type[] { typeof(UrlIdentifier), typeof(int) }, null);
+                    __result = (UrlDir)createDirectory.Invoke(dir, new object[] { id, index + 1 });
+                    return false; // Skip the original method.
+                }
+                j++;
+            }
+            __result = null;
+            return false; // Skip the original method.
+        }
+
+        public static bool Patch04(UrlDir __instance, UrlIdentifier id, int index, ref UrlConfig __result)
+        {
+            if (index == id.urlDepth)
+            {
+                int i = 0;
+                int count = __instance.files.Count;
+                while (i < count)
+                {
+                    UrlFile urlFile = __instance.files[i];
+                    if (urlFile.fileType == FileType.Config && urlFile.ContainsConfig(id[index]))
+                    {
+                        __result = urlFile.GetConfig(id[index]);
+                        return false; // Skip the original method.
+                    }
+                    i++;
+                }
+            }
+            else
+            {
+                int j = 0;
+                int count2 = __instance.children.Count;
+                while (j < count2)
+                {
+                    var getConfig = typeof(UrlDir).GetMethod("GetConfig", BindingFlags.Instance | BindingFlags.NonPublic, null, new Type[] { typeof(UrlIdentifier), typeof(int) }, null);
+                    UrlDir urlDir = __instance.children[j];
+                    if (urlDir.name != string.Empty && id[index] == urlDir.name)
+                    {
+
+                        __result = (UrlConfig)getConfig.Invoke(urlDir, new object[] { id, index + 1 });
+                        return false; // Skip the original method.
+                    }
+                    else
+                    {
+                        UrlConfig config = (UrlConfig)getConfig.Invoke(urlDir, new object[] { id, index });
+                        if (config != null)
+                        {
+                            __result = config;
+                            return false; // Skip the original method.
+                        }
+                    }
+                    j++;
+                }
+
+                int k = 0;
+                int count3 = __instance.files.Count;
+                while (k < count3)
+                {
+                    UrlFile urlFile2 = __instance.files[k];
+                    if (urlFile2.fileType == FileType.Config && id[index] == urlFile2.name)
+                    {
+                        __result = urlFile2.GetConfig(id[index + 1]);
+                        return false; // Skip the original method.
+                    }
+                    k++;
+                }
+            }
+            __result = null;
+            return false; // Skip the original method
         }
     }
+
+
 }
--- README.md
+++ README.md
@@ -7,17 +7,31 @@ Please submit [issues][iss] and [patches][patch] through [SourceHut](https://sr.
 [iss]: https://todo.sr.ht/~thepuzzlemaker/KSP-AlphaPatch
 [patch]: https://lists.sr.ht/~thepuzzlemaker/misc-projects
 
-# Installation
+# Installation (Windows)
 
-1. Download [BepInEx v5.4.22](https://github.com/BepInEx/BepInEx/releases/tag/v5.4.22) from its GitHub page. Extract into the KSP directory. Ensure that `winhttp.dll` is replaced.
-2. Start the game, then close it. It does not need to load all the way, only to the point where the Squad logo shows.
-3. Download (or build) the `AlphaPatch.dll` file and place it in `BepInEx/patchers` directory.
+1. Download [BepInEx v5.4.22](https://github.com/BepInEx/BepInEx/releases/tag/v5.4.22) from its GitHub page, specifically `BepInEx_x64_5.4.22.0.zip`. Extract only `winhttp.dll`. **Do not extract the BepInEx folder, or `doorstop_config.ini`.
+2. Download `doorstop_config.ini`.
+3. Download (or build) `AlphaPatch.exe` file and place it in the root KSP directory.
+4. Ensure [Harmony](https://github.com/KSPModdingLibs/HarmonyKSP) is installed. This is a common dependency of other mods, so it likely is already.
+4. Run the game as normal.
+
+# Installation (Linux, MacOS, etc.)
+
+> NOTE: This setup is currently untested. Let me know if it gives you any issues, but it *should* work.
+
+1. Download [BepInEx v5.4.22](https://github.com/BepInEx/BepInEx/releases/tag/v5.4.22) from its GitHub page, specifically `BepInEx_unix_5.4.22.0.zip`. Extract only the `doorstop_libs` folder. **Do not extract the BepInEx folder, or `run_bepinex.sh`. 
+2. Download `run_bepinex.sh`
+3. Navigate to your KSP directory in the terminal and run `chmod +x run_bepinex.sh`.
+4. Download (or build) `AlphaPatch.exe` file and place it in the root KSP directory.
+5. Edit the line `executable_name="NAMEHERE"`, replacing `NAMEHERE` with the executable name. On Linux this is typically `KSP.x86_64`. (I don't know what it is on MacOS, let me know if you do.) 
+6. Ensure [Harmony](https://github.com/KSPModdingLibs/HarmonyKSP) is installed. This is a common dependency of other mods, so it likely is already.
+7. Run the game with `run_bepinex.sh`. In CKAN you can set the command like to be `./run_bepinex.sh -singleinstance` (and any custom command line arguments as you wish).
 
 ## Why BepInEx?
 
 This project uses BepInEx because KSP's stock modding facilities do not allow the DLL to be loaded early enough to stop out-of-order patching.
 
-BepInEx's preloader ability allows AlphaPatch to be loaded before anything in KSP, allowing it to patch the file loading code before it can load files out of order.
+BepInEx's "doorstop" patch allows AlphaPatch to be loaded before anything in KSP, allowing it to patch the file loading code before it can load files out of order.
 
 # License
 
